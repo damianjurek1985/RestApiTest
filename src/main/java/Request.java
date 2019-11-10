f033036f0446d13e42e3b8a9364aeed8e2d9ef95
damianjurek1985 <damianjurek85@gmail.com>
2019-11-10 16:19:54 +0100

    add simply post and get method

---
+++ src/main/java/Request.java
@@ -0,0 +1,49 @@
+import org.json.JSONObject;
+
+import org.apache.commons.io. IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Request {
+
+    public static void main(String[] args) {
+
+        Post_JSON();
+    }
+
+    public static void Post_JSON() {
+        String query_url = "http://localhost/phpmyadmin/sql.php?db=ksiegarnia&goto=db_structure.php&table=klienci&pos=0";
+        String json = "{ \"method\" : \"guru.test\", \"params\" : [ \"jinu awad\" ], \"id\" : 123 }";
+        try {
+            URL url = new URL(query_url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("GET");
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes("UTF-8"));
+            os.close();
+            // read the response
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+
+            String result = IOUtils.toString(in, "UTF-8");
+
+            System.out.println(result);
+            System.out.println("result after Reading JSON Response");
+            JSONObject myResponse = new JSONObject(result);
+            System.out.println("jsonrpc- "+myResponse.getString("jsonrpc"));
+            System.out.println("id- "+myResponse.getInt("id"));
+            System.out.println("result- "+myResponse.getString("result"));
+            in.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
